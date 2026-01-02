56678c25bff84b69326a1242257c9a4abe37414d
Abhilaksh-sharma-xcal <abhilaksh.sharma@xcaliber.health>
2026-01-02 14:48:44 +0530

    bind inside the bind

--- demo/main.py
+++ demo/main.py
@@ -2,3 +2,17 @@ from ray import serve
 from service_1.deployment import HelloService0
 
 app = HelloService0.bind()
+
+
+@serve.deployment
+class Binder:
+    def __init__(self, msg):
+        print("HelloService initialized")
+        from service_1.deployment import HelloService1
+        self.msg.bind()
+
+    async def __call__(self, request):
+        return {"message": "Hello from repo-hello-service"}
+
+
+app = Binder.bind()
