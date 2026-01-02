95e226e44ecd30406d7cf301084e18cf350fa434
Abhilaksh-sharma-xcal <abhilaksh.sharma@xcaliber.health>
2026-01-02 14:52:07 +0530

    dynamically binding

--- demo/main.py
+++ demo/main.py
@@ -1,18 +1,14 @@
 from ray import serve
-from service_1.deployment import HelloService0
-
-app = HelloService0.bind()
-
+from service_1.deployment import HelloService0, HelloService1
 
 @serve.deployment
 class Binder:
-    def __init__(self, msg):
-        print("HelloService initialized")
-        from service_1.deployment import HelloService1
-        self.msg.bind()
+    def __init__(self, service0_handle, service1_handle):
+        self.service0 = service0_handle
+        self.service1 = service1_handle
 
     async def __call__(self, request):
-        return {"message": "Hello from repo-hello-service"}
-
-
-app = Binder.bind()
+        # Example: call one of the services dynamically
+        result0 = await self.service0.remote(request)
+        result1 = await self.service1.remote(request)
+        return {"service0": result0, "service1": result1}
